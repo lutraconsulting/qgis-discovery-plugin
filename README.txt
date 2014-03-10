4e2dfccd671f57cd01f86b4bc6aa72d46d3bb991
Tim Martin <tjmgis@gmail.com>
2014-03-10 16:48:41 +0000

    Almost working version

---
+++ README.txt
@@ -0,0 +1,34 @@
+Plugin Builder Results
+
+Your plugin PostGISSearch was created in:
+    C:\Projects\PostGIS_Search_Tool\PostGISSearch\plugin\PostGISSearch
+
+Your QGIS plugin directory is located at:
+    C:/Users/tjmartin/.qgis2/python/plugins
+
+What's Next:
+
+  * Copy the entire directory containing your new plugin to the QGIS plugin
+    directory
+
+  * Compile the ui file using pyuic4
+
+  * Compile the resources file using pyrcc4
+
+  * Test the plugin by enabling it in the QGIS plugin manager
+
+  * Customize it by editing the implementation file:
+         postgissearch.py
+
+  * Create your own custom icon, replacing the default icon.png
+
+  * Modify your user interface by opening postgissearch.ui
+    in Qt Designer (don't forget to compile it with pyuic4 after changing it)
+
+  * You can use the Makefile to compile your Ui and resource files when
+    you make changes. This requires GNU make (gmake)
+
+For more information, see the PyQGIS Developer Cookbook at:
+http://www.qgis.org/pyqgis-cookbook/index.html
+
+(C) 2011-2014 GeoApt LLC - geoapt.com
---
+++ postgissearchdialog.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ PostGISSearchDialog
+                                 A QGIS plugin
+ Plugin for searching data in PostGIS Database
+                             -------------------
+        begin                : 2014-03-07
+        copyright            : (C) 2014 by Tim Martin
+        email                : tjmgis@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from PyQt4 import QtCore, QtGui
+from ui_postgissearch import Ui_PostGISSearch
+
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+
+
+class PostGISSearchDialog(QtGui.QDialog, Ui_PostGISSearch):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+
+
+
+
+
