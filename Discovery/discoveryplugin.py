70428017f1b9c2b3dfe12b0d1f6dc57e1181efc5
vsklencar <viktor.sklencar@lutraconsulting.co.uk>
2018-10-04 19:34:27 +0200

    WIP

--- Discovery/discoveryplugin.py
+++ Discovery/discoveryplugin.py
@@ -32,6 +32,7 @@ from qgis.core import (
     QgsFields,
     QgsGeometry,
     QgsRectangle,
+    QgsVectorLayer,
     QgsWkbTypes
 )
 from qgis.gui import QgsVertexMarker, QgsFilterLineEdit, QgsRubberBand
@@ -268,7 +269,9 @@ class DiscoveryPlugin:
             self.clear_suggestions()
             return
 
-        if self.data_type == config_dialog.DataType.POSTGRES.value:
+        print(self.data_type)
+        print(config_dialog.DataType.POSTGRES.value)
+        if str(self.data_type) == str(config_dialog.DataType.POSTGRES.value):
             query_text, query_dict = dbutils.get_search_sql(
                                         new_search_text,
                                         self.postgisgeomcolumn,
@@ -280,10 +283,27 @@ class DiscoveryPlugin:
                                         self.postgistable)
             self.schedule_search(query_text, query_dict)
 
-        elif self.data_type == config_dialog.DataType.GPKG.value:
-            #gpkg_utils.get_search_sql("/Users/vsklencar/lutra/plugins/data/discovery_test1.gpkg")
-            print("GPKG search")
-            gpkg_utils.search_sqlite(self.file)
+        elif str(self.data_type) == str(config_dialog.DataType.GPKG.value):
+            display_fields = self.postgisdisplaycolumn.split(",")
+            print(display_fields)
+            result = gpkg_utils.search_gpkg(new_search_text, self.postgissearchcolumn, display_fields, self.layer)
+            print(len(result))
+            suggestions = []
+            self.search_results = []
+
+            for row in result:
+                geom, epsg, suggestion_text = row[0], row[1], ", ".join(row[2])
+                extra_data = {}
+                # TODO @vsklencar
+                # for idx, extra_col in enumerate(self.extra_expr_columns):
+                #     extra_data[extra_col] = row[3 + idx]
+                self.search_results.append((geom, epsg, extra_data))
+                suggestions.append(suggestion_text)
+                #
+            model = self.completer.model()
+            model.setStringList(suggestions)
+            self.completer.complete()
+
 
     def do_db_operations(self):
         if self.next_query_time is not None and self.next_query_time < time.time():
@@ -297,8 +317,7 @@ class DiscoveryPlugin:
                 self.db_conn = None
 
     def perform_search(self):
-        # TODO
-        #if self.data_type == config_dialog.DataType.POSTGRES.value:
+
         cur = self.get_db_cur()
         cur.execute(self.query_sql, self.query_dict)
 
@@ -311,7 +330,7 @@ class DiscoveryPlugin:
                 extra_data[extra_col] = row[3+idx]
             self.search_results.append((geom, epsg, extra_data))
             suggestions.append(suggestion_text)
-
+        print("suggestions")
         model = self.completer.model()
         model.setStringList(suggestions)
         self.completer.complete()
@@ -327,6 +346,7 @@ class DiscoveryPlugin:
         self.select_result(self.search_results[result_index.row()])
 
     def select_result(self, result_data):
+        print("select_result")
         geometry_text, src_epsg, extra_data = result_data
         location_geom = QgsGeometry.fromWkt(geometry_text)
         canvas = self.iface.mapCanvas()
@@ -427,8 +447,9 @@ class DiscoveryPlugin:
         self.make_enabled(False)   # assume the config is invalid first
 
         self.db_conn = None
-        if self.data_type == config_dialog.DataType.POSTGRES.value:
+        if str(self.data_type) == str(config_dialog.DataType.POSTGRES.value):
             self.conn_info = dbutils.get_postgres_conn_info(connection)
+            self.layer = None
 
             if len(connection) == 0 or len(self.postgisschema) == 0 or len(self.postgistable) == 0 or \
                     len(self.postgissearchcolumn) == 0 or len(self.postgisgeomcolumn) == 0:
@@ -438,8 +459,8 @@ class DiscoveryPlugin:
                 iface.messageBar().pushMessage("Discovery", "The database connection '%s' does not exist!" % connection,
                                                level=Qgis.Critical)
                 return
-        elif self.data_type == config_dialog.DataType.GPKG.value:
-            #self.layer = QgsVectorLayer(self.file)
+        elif str(self.data_type) == str(config_dialog.DataType.GPKG.value):
+            self.layer =QgsVectorLayer(self.file + '|layername=' + self.postgistable, self.postgistable, 'ogr')
             self.conn_info = None
         self.extra_expr_columns = []
         self.scale_expr = None
--- Discovery/gpkg_utils.py
+++ Discovery/gpkg_utils.py
@@ -1,6 +1,6 @@
 from osgeo import ogr, gdal
-from qgis.core import QgsVectorLayer, QgsDataSourceUri
-from qgis.PyQt.QtSql import QSqlDatabase
+from qgis.core import QgsVectorLayer, QgsDataSourceUri, QgsFeatureRequest, QgsExpression
+from qgis.PyQt.QtSql import QSqlDatabase, QSqlRecord
 
 def list_gpkg_layers(pckg_path):
     layer_names = []
@@ -30,10 +30,59 @@ def list_gpkg_fields(gpkg_path, name, bar_warning=None):
         return []
 
 
-def get_search_sql(gpkg_path):
+def search_gpkg(search_text, search_field, display_fields, layer):
+    expr_str = "{0} ILIKE '%{1}%'".format(search_field, search_text)
+    expr = QgsExpression(expr_str)
+    it = layer.getFeatures(QgsFeatureRequest(expr))
+    result = []
+
+    for f in it:
+        featureInfo = []
+        geom = f.geometry().asWkt()
+        epsg = layer.crs().authid()
+        featureInfo.append(geom)
+        featureInfo.append(epsg)
+        extra_info = []
+        for fields in display_fields:
+            extra_info.append(f[fields])
+        featureInfo.append(extra_info)
+        result.append(featureInfo)
+    return result
+
+
+# DEPRECATED
+def get_search_sql():
+    query = """SELECT
+                GeomFromWKB(geom) AS geom,
+                 "NAME1"
+                                       || CASE WHEN "NAME1" IS NOT NULL THEN
+                                                     ', ' || "NAME1"
+                                                 ELSE
+                                                     ''
+                                                 END
+                                            || CASE WHEN "COUNTRY" IS NOT NULL THEN
+                                                     ', ' || "COUNTRY"
+                                                 ELSE
+                                                     ''
+                                                 END
+                                            AS suggestion_string 
+                  FROM
+                        test1
+                     WHERE
+                        "NAME1" LIKE
+                     '%ROAD%'
+                  ORDER BY
+                        "NAME1"
+                    LIMIT 10;"""
+    return query, {}
+
+
+def get_search_sql2(search_text, search_field, display_fields, gpkg_path):
     gpkg_dr = ogr.GetDriverByName( 'GPKG' )
     gpkg_ds = gpkg_dr.Open(gpkg_path, update=1)
-    result_set = gpkg_ds.ExecuteSQL("SELECT * FROM test1;")
+    query = "SELECT {0} FROM test1 WHERE {0} LIKE '%{1}%';".format(search_field, search_text)
+    print(query)
+    result_set = gpkg_ds.ExecuteSQL(query)
     f = result_set.GetNextFeature()
     geom_read = f.GetGeometryRef()
     print(geom_read)
@@ -45,24 +94,107 @@ def get_search_sql(gpkg_path):
     gpkg_ds.ReleaseResultSet(result_set)
     pass
 
+
 # TODO @vsklencar
-def search_sqlite(path):
-    # Get file path
-    #pass
-    #uri = QgsDataSourceUri(layer.dataProvider().dataSourceUri())
+def search_sqlite(search_text, search_field, display_fields, gpkg_path):
+    print(search_sqlite)
     # Create DB connexion to do SQL
-    db = QSqlDatabase.addDatabase("QSQLITE");
+    db = QSqlDatabase.addDatabase("QSQLITE")
     # Reuse the path to DB to set database name
-    db.setDatabaseName(path)
+    db.setDatabaseName(gpkg_path)
     # Open the connection
     db.open()
+
+    db.exec_("""SELECT load_extension('libspatialite-4.dll')""")
+    geom_column = "geom"
     # query the table
-    query = db.exec_("""select * from test1""")
-    # Play with results (not efficient, just for demo)
+    query_text = """ SELECT
+                            name1, 
+                            ST_AsText("%s") AS geom
+                            FROM test1
+                            LIMIT 10;
+                     """ % (geom_column)
+    print(query_text)
+    query = db.exec_(query_text)
+    print(query.lastError().text())
+
+    if query.lastError().text():
+        return []
+
+    results = []
     while query.next():
-        values = []
         record = query.record()
-        for index in range(record.count()):
-            # We exclude the geometry to join attributes data
-            values.append(str(record.value(index)))
-        print (';').join(values)
+        print(type(record))
+        print(record)
+
+        results.append(record.field('geom'))
+        # for index in display_fields:
+        #     # We exclude the geometry to join attributes data
+        #     values.append(str(record.value(index)))
+    print(results)
+    return results
+
+
+def spatialite_connect(path):
+    import sqlite3
+    print("spatialite_connect")
+    con = sqlite3.connect(path)
+    con.enable_load_extension(True)
+    cur = con.cursor()
+    libs = [
+        # SpatiaLite >= 4.2 and Sqlite >= 3.7.17, should work on all platforms
+        ("mod_spatialite", "sqlite3_modspatialite_init"),
+        # SpatiaLite >= 4.2 and Sqlite < 3.7.17 (Travis)
+        ("mod_spatialite.so", "sqlite3_modspatialite_init"),
+        # SpatiaLite < 4.2 (linux)
+        ("libspatialite.so", "sqlite3_extension_init")
+        ]
+    found = False
+    for lib, entry_point in libs:
+        try:
+            print("lib trying " + str(lib))
+            cur.execute("select load_extension('{}', '{}')".format(lib, entry_point))
+        except sqlite3.OperationalError:
+            continue
+        else:
+            found = True
+            break
+    if not found:
+        raise RuntimeError("Cannot find any suitable spatialite module")
+    cur.close()
+    #con.enable_load_extension(False)
+    return con
+
+def spatialite_connect2(*args, **kwargs):
+    """returns a dbapi2.Connection to a SpatiaLite db
+using the "mod_spatialite" extension (python3)"""
+    import sqlite3
+    con = sqlite3.dbapi2.connect(*args, **kwargs)
+    con.enable_load_extension(True)
+    cur = con.cursor()
+    libs = [
+        # SpatiaLite >= 4.2 and Sqlite >= 3.7.17, should work on all platforms
+        ("mod_spatialite", "sqlite3_modspatialite_init"),
+        # SpatiaLite >= 4.2 and Sqlite < 3.7.17 (Travis)
+        ("mod_spatialite.so", "sqlite3_modspatialite_init"),
+        # SpatiaLite < 4.2 (linux)
+        ("libspatialite.so", "sqlite3_extension_init")
+    ]
+    found = False
+    for lib, entry_point in libs:
+        try:
+            cur.execute("select load_extension('{}', '{}')".format(lib, entry_point))
+        except sqlite3.OperationalError:
+            continue
+        else:
+            found = True
+            break
+    if not found:
+        raise RuntimeError("Cannot find any suitable spatialite module")
+    cur.close()
+    con.enable_load_extension(False)
+    return con
+
+
+
+
