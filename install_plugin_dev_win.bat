2e0edc14f507c644655c37d2cb4ac03cff0f6a65
radek.pasiok <radek.pasiok@lutraconsulting.co.uk>
2023-06-12 12:27:05 +0200

    Handle PostgreSQL connection timeouts.

--- install_plugin_dev_win.bat
+++ install_plugin_dev_win.bat
@@ -1,10 +1,11 @@
 
 SET DIR=%cd%
+SET QGIS_PROFILE=default
 
 SET PLUGIN=Discovery
 SET SRC=%DIR%\%PLUGIN%
 
-SET DEST=%UserProfile%\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins
+SET DEST=%UserProfile%\AppData\Roaming\QGIS\QGIS3\profiles\%QGIS_PROFILE%\python\plugins
 
 SET DEST_PLUGIN=%DEST%\%PLUGIN%
 
